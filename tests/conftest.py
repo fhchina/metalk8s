614762cea1154a57d658a5f3f964ac7f3e5d3dea
Sylvain Laperche <sylvain.laperche@scality.com>
2019-04-25 09:51:11 +0200

    tests: move fixtures to the upper level
    They will be reused for cluster expansion tests.
    
    Refs: #982
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pathlib
 
 import pytest
 from pytest_bdd import given, then
+import yaml
 
 
 # Pytest command-line options
@@ -14,6 +15,37 @@ def pytest_addoption(parser):
         help="Root of the ISO file tree."
     )
 
+# Fixtures {{{
+
+@pytest.fixture(scope="module")
+def version(request, host):
+    iso_root = request.config.getoption("--iso-root")
+    product_path = iso_root / "product.txt"
+    with host.sudo():
+        return host.check_output(
+            'source %s && echo $SHORT_VERSION', str(product_path)
+        )
+
+@pytest.fixture(scope="module")
+def kubeconfig_data(request, host):
+    """Fixture to generate a kubeconfig file for remote usage."""
+    with host.sudo():
+        kubeconfig_file = host.file('/etc/kubernetes/admin.conf')
+        if not kubeconfig_file.exists:
+            pytest.skip(
+                "Must be run on bootstrap node, or have an existing file at "
+                "/etc/kubernetes/admin.conf"
+            )
+        return yaml.safe_load(kubeconfig_file.content_string)
+
+
+@pytest.fixture
+def kubeconfig(kubeconfig_data, tmp_path):
+    kubeconfig_path = tmp_path / "admin.conf"
+    kubeconfig_path.write_text(yaml.dump(kubeconfig_data), encoding='utf-8')
+    return str(kubeconfig_path)  # Need Python 3.6 to open() a Path object
+
+# }}}
 # Given {{{
 
 @given("the Kubernetes API is available")
--- tests/post/steps/conftest.py
+++ tests/post/steps/conftest.py
@@ -6,40 +6,8 @@ from pytest_bdd import (
     when,
     parsers,
 )
-import yaml
 
 from tests import kube_utils
-
-
-# Pytest fixtures
-@pytest.fixture(scope="module")
-def kubeconfig_data(request, host):
-    """Fixture to generate a kubeconfig file for remote usage."""
-    with host.sudo():
-        kubeconfig_file = host.file('/etc/kubernetes/admin.conf')
-        if not kubeconfig_file.exists:
-            pytest.skip(
-                "Must be run on bootstrap node, or have an existing file at "
-                "/etc/kubernetes/admin.conf"
-            )
-        return yaml.safe_load(kubeconfig_file.content_string)
-
-
-@pytest.fixture
-def kubeconfig(kubeconfig_data, tmp_path):
-    kubeconfig_path = tmp_path / "admin.conf"
-    kubeconfig_path.write_text(yaml.dump(kubeconfig_data), encoding='utf-8')
-    return str(kubeconfig_path)  # Need Python 3.6 to open() a Path object
-
-
-@pytest.fixture
-def version(request, host):
-    iso_root = request.config.getoption("--iso-root")
-    product_path = iso_root / "product.txt"
-    with host.sudo():
-        return host.check_output(
-            'source %s && echo $SHORT_VERSION', str(product_path)
-        )
 
 
 def _run_bootstrap(request, host):
