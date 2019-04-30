ba3ed244fa5f01f7c57890ee4ef435a2c8c77299
Sylvain Laperche <sylvain.laperche@scality.com>
2019-04-30 17:21:52 +0200

    test: refactor pod listing
    In `post` tests we were checking running pods, but I also re-implemented
    it for `install` tests…
    
    Code duplication being bad, let's refactor this.
    The `post` tests were calling `kubectl`, whereas my tests where using
    the API directly.
    
    So, I rewrote the existing helper (using API) and also added the ability
    to filter by node (which allows to use `==` instead of `>=`).
    
    Refs: #982
    Signed-off-by: Sylvain Laperche <sylvain.laperche@scality.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,11 @@ import pathlib
 
 import kubernetes as k8s
 import pytest
-from pytest_bdd import given, then
+from pytest_bdd import given, parsers, then
 import yaml
+
+from tests import kube_utils
+from tests import utils
 
 
 # Pytest command-line options
@@ -67,6 +70,20 @@ def check_service(host):
 @then("the Kubernetes API is available")
 def verify_kubeapi_service(host):
     _verify_kubeapi_service(host)
+
+
+@then(parsers.parse(
+    "we have {pods_count:d} running pod labeled '{label}' on node '{node}'"
+))
+def count_running_pods(k8s_client, pods_count, label, node):
+    def _check_pods_count():
+        pods = kube_utils.get_pods(
+            k8s_client, label, node, namespace="kube-system", state="Running",
+        )
+        assert len(pods) == pods_count
+
+    utils.retry(_check_pods_count, times=10, wait=3)
+
 
 # }}}
 # Helpers {{{
